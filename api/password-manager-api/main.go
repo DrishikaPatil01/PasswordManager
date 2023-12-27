374346d0fa59376f598d6ac05e22393700fdd1fa
Harshit Cd <harshitcd@gmail.com>
2023-12-27 21:28:30 +0530

    Adds the basic structure for api

--- api/password-manager-api/main.go
+++ api/password-manager-api/main.go
@@ -13,5 +13,16 @@ func main() {
 	router.Use(cors.Default()) // remove this in production.
 
 	router.GET("/health", handlers.HealthCheck)
+	router.PUT("/signup", handlers.HealthCheck)
+	router.POST("/forgot-password", handlers.HealthCheck)
+	router.PUT("/login", handlers.HealthCheck)
+	router.DELETE("/user/logout", handlers.HealthCheck)
+	router.POST("/user/reset-password", handlers.HealthCheck)
+
+	router.GET("/user/credentials", handlers.HealthCheck)
+	router.DELETE("/user/credentials", handlers.HealthCheck)
+	router.PUT("/user/credentials", handlers.HealthCheck)
+	router.POST("/user/credentials", handlers.HealthCheck)
+
 	router.Run(":8080")
 }
